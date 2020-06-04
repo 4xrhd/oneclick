1fa79ff5cb0b1274a7841b395987b2391d024a97
4xrhd <65583096+4xrhd@users.noreply.github.com>
2020-06-04 11:07:26 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+cd 
 echo "Install all tools in one click ...";
 sleep 2;
 cat go.txt | xargs -n1 -I a go get -v -u github.com/a 
-
+mkdir tools/
+cd
+cd tools
 cat bb.txt | xargs -n1 -I a sh -c "git clone https://github.com/a"
 cat htb.txt | xargs -n1 -I a sh -c "git clone https://github.com/a"
